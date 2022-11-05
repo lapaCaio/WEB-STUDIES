8db765ec9727ee67396f6ad486ca121762bd8592
lapaCaio <lapacaio2001@gmail.com>
2022-11-04 21:06:44 -0300

    UPD - scripts.js

--- ONLY_HTML/TESTS/SCRIPT/scripts.js
+++ ONLY_HTML/TESTS/SCRIPT/scripts.js
@@ -3,6 +3,24 @@ function readMore(){
     var moreText = document.getElementById("more");
     var btnReadMore = document.getElementById("btnReadMore");
 
+    //talvez usar o this e passar algo como parametro...
+
+
+    if(dots.style.display ===  "none"){
+        dots.style.display="inline";
+        moreText.style.display="none";
+        btnReadMore.innerHTML="Read More"
+    }else{
+        dots.style.display="none";
+        moreText.style.display="inline";
+        btnReadMore.innerHTML="No Read More"
+    }
+}
+
+function readMoreII(d, m, b){
+    var dots = document.getElementById(d);
+    var moreText = document.getElementById(m);
+    var btnReadMore = document.getElementById(b);
 
     if(dots.style.display ===  "none"){
         dots.style.display="inline";
