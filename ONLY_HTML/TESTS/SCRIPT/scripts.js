5cb43aff54ee3f94afc49056f801e6f8f7208a22
lapaCaio <lapacaio2001@gmail.com>
2022-11-04 21:21:20 -0300

    ...

--- ONLY_HTML/TESTS/SCRIPT/scripts.js
+++ ONLY_HTML/TESTS/SCRIPT/scripts.js
@@ -1,4 +1,4 @@
-function readMore(){
+function readMore() {
     var dots = document.getElementById("dots");
     var moreText = document.getElementById("more");
     var btnReadMore = document.getElementById("btnReadMore");
@@ -6,29 +6,29 @@ function readMore(){
     //talvez usar o this e passar algo como parametro...
 
 
-    if(dots.style.display ===  "none"){
-        dots.style.display="inline";
-        moreText.style.display="none";
-        btnReadMore.innerHTML="Read More"
-    }else{
-        dots.style.display="none";
-        moreText.style.display="inline";
-        btnReadMore.innerHTML="No Read More"
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        moreText.style.display = "none";
+        btnReadMore.innerHTML = "Read More"
+    } else {
+        dots.style.display = "none";
+        moreText.style.display = "inline";
+        btnReadMore.innerHTML = "No Read More"
     }
 }
 
-function readMoreII(d, m, b){
+function readMoreII(d, m, b) {
     var dots = document.getElementById(d);
     var moreText = document.getElementById(m);
     var btnReadMore = document.getElementById(b);
 
-    if(dots.style.display ===  "none"){
-        dots.style.display="inline";
-        moreText.style.display="none";
-        btnReadMore.innerHTML="Read More"
-    }else{
-        dots.style.display="none";
-        moreText.style.display="inline";
-        btnReadMore.innerHTML="No Read More"
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        moreText.style.display = "none";
+        btnReadMore.innerHTML = "Read More"
+    } else {
+        dots.style.display = "none";
+        moreText.style.display = "inline";
+        btnReadMore.innerHTML = "No Read More"
     }
 }
